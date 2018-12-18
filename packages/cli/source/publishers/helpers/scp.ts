dddfac08564b84d64a922ee9b337f5e883cc66bc
ivebencrazy <ivebencrazy@users.noreply.github.com>
2018-12-17 22:10:19 -0800

    Add scp utility

---
+++ packages/cli/source/publishers/helpers/scp.ts
@@ -0,0 +1,25 @@
+/** @source https://github.com/ecto/node-scp */
+
+import { promisify } from "@civility/utilities"
+import { exec as execModule } from "child_process"
+
+const exec = promisify(execModule)
+
+export interface ISCPOptions {
+  file: string
+  host: string
+  path: string
+  port?: string
+  user?: string
+}
+
+export function send({ file, host, path, port = "22", user }: ISCPOptions) {
+  const url = (user == null ? "" : user + "@") + host + ":" + path
+  return exec(`scp -r -P ${port} -o "ControlMaster no" ${file} ${url}`)
+}
+
+
+export function get({ file, host, path, port = "22", user }: ISCPOptions) {
+  const url = (user === undefined ? "" : user + "@") + host + ":" + file
+  return exec(`scp -r -P ${port} -o "ControlMaster no" ${url} ${path}`)
+}
